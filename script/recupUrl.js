1dbf56ad673d510d3133e11dbde2926e6439cfe4
michael <michaelbecquer@yahoo.fr>
2022-03-08 16:06:20 +0100

    finish

--- script/recupUrl.js
+++ script/recupUrl.js
@@ -1,10 +1,26 @@
 const recupNom = document.getElementById("recupNom");
+const recupPrenom = document.getElementById("recupPrenom");
 //recuperation de l'url a la quelle on garde que les parametres
 const search = window.location.search.substring(1);
+const form = document.forms[0];
+const choice = document.getElementById("choice");
 
 const recup = search.split("&");
-const splite = recup[0].split("=");
-// console.log("search", search);
-// console.log("recup", recup);
+// const splitNom = recup[0].split("=");
+// const splitPrenom = recup[1].split("=");
+// const splitFormation = recup[2].split("=");
+// const splitHeureDebut = recup[3].split("=");
+// const splitHeureFin = recup[4].split("=");
+// const splitDateDebut = recup[5].split("=");
+// const splitDateFin = recup[6].split("=");
+// const splitNombreJour = recup[7].split("=");
+// const splitChoice = recup[8].split("=");
+recup.forEach((el, index) => {
+  let splitted = el.split("=");
+  form[index].value = splitted[1];
+});
 
-recupNom.value = splite[1];
+// console.log(recup);
+// recupNom.value = splitNom[1];
+// recupPrenom.value = splitPrenom[1];
+// choice.innerText = splitChoice;
